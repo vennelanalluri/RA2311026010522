46e438e086c92dae230c3c9d58fd175d1ec4ac72
vennelanalluri <158325835+vennelanalluri@users.noreply.github.com>
2026-05-02 12:06:00 +0530

    added stage 6 priority logic

--- vehicle_maintenance_scheduler/priority_notifications.py
+++ vehicle_maintenance_scheduler/priority_notifications.py
@@ -1,12 +1,6 @@
 import requests
 from datetime import datetime
 
-priority = {
-    "Placement": 3,
-    "Result": 2,
-    "Event": 1
-}
-
 def get_notifications(token):
     url = "http://20.207.122.201/evaluation-service/notifications"
 
@@ -15,7 +9,13 @@ def get_notifications(token):
     }
 
     response = requests.get(url, headers=headers)
-    data = response.json()["notifications"]
+    data = response.json().get("notifications", [])
+
+    priority = {
+        "Placement": 3,
+        "Result": 2,
+        "Event": 1
+    }
 
     for n in data:
         n["time"] = datetime.strptime(n["Timestamp"], "%Y-%m-%d %H:%M:%S")
@@ -29,7 +29,7 @@ def get_notifications(token):
 
 
 if __name__ == "__main__":
-    token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...."
+    token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJ2ZW5uZWxhbmFsbHVyaUBnbWFpbC5jb20iLCJleHAiOjE3Nzc3MDQzMDAsImlhdCI6MTc3NzcwMzQwMCwiaXNzIjoiQWZmb3JkIE1lZGljYWwgVGVjaG5vbG9naWVzIFByaXZhdGUgTGltaXRlZCIsImp0aSI6ImMzYjc4YjU5LTY3MGEtNDU3ZC05Mjg1LTBjMjEyMDhmM2E0OSIsImxvY2FsZSI6ImVuLUlOIiwibmFtZSI6InZlbm5lbGEiLCJzdWIiOiI2OGU2NWRkOS0wOGRiLTQxMjItYWM0Ni0xZmJhZGZiNzc1MGQifSwiZW1haWwiOiJ2ZW5uZWxhbmFsbHVyaUBnbWFpbC5jb20iLCJuYW1lIjoidmVubmVsYSIsInJvbGxObyI6InJhMjMxMTAyNjAxMDUyMiIsImFjY2Vzc0NvZGUiOiJRa2JweEgiLCJjbGllbnRJRCI6IjY4ZTY1ZGQ5LTA4ZGItNDEyMi1hYzQ2LTFmYmFkZmI3NzUwZCIsImNsaWVudFNlY3JldCI6ImdaSG1zTmVGYUJiZGZWRHUifQ._tNu5kj6ol3i6XN41DymfQWQqGvNIh2K7plEeaHp4Q0",
 
     result = get_notifications(token)
 
